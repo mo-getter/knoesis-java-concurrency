0a04937cee7b13c83daf0ef1fe722c7986a572c6
Alan Smith <alan@knoesis.org>
2014-01-17 17:35:30 -0500

    reimplement Parallel.forEach() to use ProducerConsumer

--- src/test/java/org/knoesis/util/concurrent/parallel/ParallelForTest.java
+++ src/test/java/org/knoesis/util/concurrent/parallel/ParallelForTest.java
@@ -1,12 +1,10 @@
 
 package org.knoesis.util.concurrent.parallel;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -31,28 +29,27 @@ public class ParallelForTest extends TestCase {
     }
     
     public void testParallelFor() throws InterruptedException {
-        List<Integer> input = new ArrayList<Integer>(NUM_ELEMENTS);
+        Set<Integer> input = new HashSet<Integer>(NUM_ELEMENTS);
         for (int i=0; i<NUM_ELEMENTS; i++) {
             input.add(i);
         }
         TestOperation operation = new TestOperation();
+        long start = System.currentTimeMillis();
         Parallel.forEach(input, operation);
+        System.out.format("Parallel.forEach took %dms\n", System.currentTimeMillis() - start);
         assertTrue(operation.getResult().containsAll(input));
     }
     
     public static class TestOperation implements Operation<Integer> {
         
-        private final List<Integer> result = new CopyOnWriteArrayList<Integer>();
+        private final Set<Integer> result = new ConcurrentSkipListSet<Integer>();
 
+        @Override
         public void perform(Integer x) {
             result.add(x);
         }
-
-        public void handleException(Throwable ex, Integer x) {
-            Logger.getLogger(TestOperation.class.getName()).log(Level.SEVERE, String.format("Exception on element %s", x), ex);
-        }
         
-        Collection<Integer> getResult() {
+        public Collection<Integer> getResult() {
             return result;
         }
         
